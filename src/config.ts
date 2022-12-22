b474db0a1469526bc0ba11ee991ea8a453c23529
MalindaK <malindakpt@gmail.com>
2022-12-22 12:01:20 +0530

    fe

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const config = {
   resetPasswordTimeout: 120 * 1000,
 
   localFEUrl: "http://localhost:3000",
-  prodFEUrl: "https://blue-rock-074bb2400.2.azurestaticapps.net",
+  prodFEUrl: "https://red-field-0788c2100.2.azurestaticapps.net",
 
   dbLogger: false,
   fromEmail: "malindakpt@gmail.com",
